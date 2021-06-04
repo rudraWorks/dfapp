c02abff23d1dc60a63d0cfa7e80f19225444ab6c
rudra pratap singh <63004704+rudraWorks@users.noreply.github.com>
2021-06-04 19:18:34 +0530

    oo

--- newapp/urls.py
+++ newapp/urls.py
@@ -24,9 +24,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('form',views.form,name='form'),
-]
-#  path('admin/', admin.site.urls),
-#     path('', include("Home.urls")),
+     path('admin/', admin.site.urls),
+    path('', include("Home.urls")),
 
-#     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-#     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
